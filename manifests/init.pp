3854f467219fbe66b8b5194aac0721a3d6babc6d
Marko Tomic <marko@markomedia.com.au>
2013-09-24 09:59:57 +1000

    initial import

---
+++ manifests/init.pp
@@ -0,0 +1,40 @@
+class phpfpm {
+	package { 'php5-fpm':
+		ensure => present,
+	}
+	package { 'php5-curl':
+		ensure => present,
+	}
+	package { 'php5-mysql':
+		ensure => present,
+	}
+	package { 'php5-gd':
+		ensure => present,
+	}
+	package { 'php-apc':
+		ensure => present,
+	}
+	package { 'php5-mcrypt':
+		ensure => present,
+	}
+	package { 'php5-cli':
+		ensure => present,
+	}
+	file { '/etc/php5/conf.d/apc.ini':
+		ensure  => file,
+		mode    => 644,
+		source  => 'puppet:///modules/phpfpm/apc.ini',
+		require => Package['php-apc'],
+	}
+	file { '/etc/php5/cli/conf.d/apc.ini':
+                ensure  => file,
+                mode    => 644,
+                source  => 'puppet:///modules/phpfpm/apc.ini',
+                require => Package['php-apc'],
+        }
+	service { 'php5-fpm':
+		ensure => running,
+		enable => true,
+		require => Package['php-apc','php5-cli','php5-mysql','php5-curl','php5-mcrypt','php5-gd'],
+	}
+}
