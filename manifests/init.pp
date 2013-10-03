0522599e7f73ebe80bedc9924944065266436e74
Marko Tomic <marko@markomedia.com.au>
2013-10-03 13:41:31 +1000

    revert php 5.4 changes

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,19 +1,5 @@
 class phpfpm {
-	exec { "add_php5_4":
-		command => "add-apt-repository ppa:ondrej/php5; apt-get update;",
-		path    => "/usr/bin/",
-		refreshonly => true,
-	}
 	package { 'php5-fpm':
-		ensure => present,
-	}
-	package { 'php-pear':
-		ensure => present,
-	}
-	package { 'php5-user-cache':
-		ensure => present,
-	}
-	package { 'php5-cgi':
 		ensure => present,
 	}
 	package { 'php5-curl':
@@ -34,20 +20,21 @@ class phpfpm {
 	package { 'php5-cli':
 		ensure => present,
 	}
-#	file { '/etc/php5/conf.d/apc.ini':
-#		ensure  => file,
-#		mode    => 644,
-#		source  => 'puppet:///modules/phpfpm/apc.ini',
-#		require => Package['php-apc'],
-#	}
-#	file { '/etc/php5/cli/conf.d/apc.ini':
-#               ensure  => file,
-#               mode    => 644,
-#                source  => 'puppet:///modules/phpfpm/apc.ini',
-#                require => Package['php-apc'],
-#        }
+	file { '/etc/php5/conf.d/apc.ini':
+		ensure  => file,
+		mode    => 644,
+		source  => 'puppet:///modules/phpfpm/apc.ini',
+		require => Package['php-apc'],
+	}
+	file { '/etc/php5/cli/conf.d/apc.ini':
+                ensure  => file,
+                mode    => 644,
+                source  => 'puppet:///modules/phpfpm/apc.ini',
+                require => Package['php-apc'],
+        }
 	service { 'php5-fpm':
 		ensure => running,
 		enable => true,
+		require => Package['php-apc','php5-cli','php5-mysql','php5-curl','php5-mcrypt','php5-gd'],
 	}
 }
