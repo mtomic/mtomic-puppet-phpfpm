438ca66b28481ea5163d574782b7bc607a5dc220
root <root@devops.mamamia.com.au>
2014-04-10 08:20:51 -0400

    add php5 apt repo

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,32 @@
-class phpfpm {
+# Class: php5-fpm
+#
+#   This module adds the latest stable PHP version (v5.5) to the target node.
+#
+#
+class php5-fpm {
+	case $::osfamily {
+	    'Debian': {
+	      $add_repo_cmd     = '/usr/bin/apt-get-repository -y ppa:ondrej/php5'
+	      $update_repos_cmd = '/usr/bin/apt-get update -y -qq'
+	
+	      exec { 'add_php_repo':
+	        command => $add_repo_cmd,
+	        notify  => Exec['update_repos'],
+	        unless  => '/usr/bin/test -f /etc/apt/sources.list.d/ondrej-php5-precise.list'
+	      }
+	      exec { 'update_repos':
+	        command     => $update_repos_cmd,
+	        require     => Exec['add_php_repo'],
+	        before      => Package['php5-fpm'],
+	        refreshonly => true,
+	      }
+	    }
+	
+	    default: {
+	      fail("The newrelic_server_monitor module does not support ${::osfamily}.")
+	    }
+	}
+
 	package { 'php5-fpm':
 		ensure => present,
 	}
@@ -11,30 +39,15 @@ class phpfpm {
 	package { 'php5-gd':
 		ensure => present,
 	}
-	package { 'php-apc':
-		ensure => present,
-	}
 	package { 'php5-mcrypt':
 		ensure => present,
 	}
 	package { 'php5-cli':
 		ensure => present,
 	}
-	file { '/etc/php5/conf.d/apc.ini':
-		ensure  => file,
-		mode    => 644,
-		source  => 'puppet:///modules/phpfpm/apc.ini',
-		require => Package['php-apc'],
-	}
-	file { '/etc/php5/cli/conf.d/apc.ini':
-                ensure  => file,
-                mode    => 644,
-                source  => 'puppet:///modules/phpfpm/apc.ini',
-                require => Package['php-apc'],
-        }
 	service { 'php5-fpm':
 		ensure => running,
 		enable => true,
-		require => Package['php-apc','php5-cli','php5-mysql','php5-curl','php5-mcrypt','php5-gd'],
+		require => Package['php5-curl','php5-mysql','php5-gd','php5-mcrypt','php5-cli'],
 	}
 }
